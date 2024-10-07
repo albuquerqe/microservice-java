fa5e14cf1c18aed257255db659f9eae57060880d
Luiz H Albuquerque <134562026+albuquerqe@users.noreply.github.com>
2024-10-07 10:05:52 -0300

    Cambio Service

---
+++ cambio-service-1/src/main/java/br/edu/atitus/paradigma/cambio_service/CambioService1Application.java
@@ -0,0 +1,13 @@
+package br.edu.atitus.paradigma.cambio_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CambioService1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CambioService1Application.class, args);
+	}
+
+}
